730cf08498be3aed11477ede3221f4a3184a0ab0
Tim Pease <tim.pease@gmail.com>
2007-12-29 20:54:48 +0000

    Fixing spec helper to work with the new logging gem dependencies

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,7 @@
 # $Id$
 
-begin
-  require 'webby'
-rescue LoadError
-  path = File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
-  raise if $:.include? path
-  $: << path
-  retry
-end
+require 'rubygems'
+require File.expand_path(
+    File.join(File.dirname(__FILE__), '..', 'lib', 'webby'))
 
 # EOF
