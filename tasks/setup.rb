da6152f3a13519207d0f53d825f0bc2239c6ffd0
Tim Pease <tim.pease@gmail.com>
2008-02-22 00:15:15 +0000

    updating rake tasks

--- tasks/setup.rb
+++ tasks/setup.rb
@@ -2,6 +2,7 @@
 
 require 'rubygems'
 require 'rake'
+require 'rake/clean'
 require 'fileutils'
 require 'ostruct'
 
@@ -16,8 +17,11 @@ PROJ.email = nil
 PROJ.url = nil
 PROJ.version = ENV['VERSION'] || '0.0.0'
 PROJ.rubyforge_name = nil
-PROJ.exclude = %w(tmp$ bak$ ~$ CVS .svn/ ^pkg/ ^doc/ announcement.txt)
+PROJ.exclude = %w(tmp$ bak$ ~$ CVS .svn/ ^pkg/ ^doc/)
 PROJ.release_name = ENV['RELEASE']
+PROJ.history_file = 'History.txt'
+PROJ.manifest_file = 'Manifest.txt'
+PROJ.readme_file = 'README.txt'
 
 # Rspec
 PROJ.specs = FileList['spec/**/*_spec.rb']
@@ -29,13 +33,16 @@ PROJ.test_file = 'test/all.rb'
 PROJ.test_opts = []
 
 # Rcov
+PROJ.rcov_dir = 'coverage'
 PROJ.rcov_opts = ['--sort', 'coverage', '-T']
+PROJ.rcov_threshold = 90.0
+PROJ.rcov_threshold_exact = false
 
 # Rdoc
 PROJ.rdoc_opts = []
 PROJ.rdoc_include = %w(^lib/ ^bin/ ^ext/ .txt$)
-PROJ.rdoc_exclude = %w(extconf.rb$ ^Manifest.txt$)
-PROJ.rdoc_main = 'README.txt'
+PROJ.rdoc_exclude = %w(extconf.rb$)
+PROJ.rdoc_main = nil
 PROJ.rdoc_dir = 'doc'
 PROJ.rdoc_remote_dir = nil
 
@@ -46,13 +53,8 @@ PROJ.libs = []
 %w(lib ext).each {|dir| PROJ.libs << dir if test ?d, dir}
 
 # Gem Packaging
-PROJ.files =
-  if test ?f, 'Manifest.txt'
-    files = File.readlines('Manifest.txt').map {|fn| fn.chomp.strip}
-    files.delete ''
-    files
-  else [] end
-PROJ.executables = PROJ.files.find_all {|fn| fn =~ %r/^bin/}
+PROJ.files = nil
+PROJ.executables = nil
 PROJ.dependencies = []
 PROJ.need_tar = true
 PROJ.need_zip = false
@@ -71,6 +73,7 @@ PROJ.svn_tags = 'tags'
 PROJ.svn_branches = 'branches'
 
 # Announce
+PROJ.ann_file = 'announcement.txt'
 PROJ.ann_text = nil
 PROJ.ann_paragraphs = []
 PROJ.ann_email = {
@@ -166,7 +169,10 @@ def depend_on( name, version = nil )
   spec = Gem.source_index.find_name(name).last
   version = spec.version.to_s if version.nil? and !spec.nil?
 
-  PROJ.dependencies << (version.nil? ? [name] : [name, ">= #{version}"])
+  PROJ.dependencies << case version
+    when nil; [name]
+    when %r/^\d/; [name, ">= #{version}"]
+    else [name, version] end
 end
 
 # Adds the given arguments to the include path if they are not already there
--- tasks/spec.rake
+++ tasks/spec.rake
@@ -1,11 +1,13 @@
 # $Id$
 
 if HAVE_SPEC_RAKE_SPECTASK
+require 'spec/rake/verify_rcov'
 
 namespace :spec do
 
   desc 'Run all specs with basic output'
   Spec::Rake::SpecTask.new(:run) do |t|
+    t.ruby_opts = PROJ.ruby_opts
     t.spec_opts = PROJ.spec_opts
     t.spec_files = PROJ.specs
     t.libs += PROJ.libs
@@ -13,6 +15,7 @@ namespace :spec do
 
   desc 'Run all specs with text output'
   Spec::Rake::SpecTask.new(:specdoc) do |t|
+    t.ruby_opts = PROJ.ruby_opts
     t.spec_opts = PROJ.spec_opts + ['--format', 'specdoc']
     t.spec_files = PROJ.specs
     t.libs += PROJ.libs
@@ -21,11 +24,19 @@ namespace :spec do
   if HAVE_RCOV
     desc 'Run all specs with RCov'
     Spec::Rake::SpecTask.new(:rcov) do |t|
+      t.ruby_opts = PROJ.ruby_opts
       t.spec_opts = PROJ.spec_opts
       t.spec_files = PROJ.specs
       t.libs += PROJ.libs
       t.rcov = true
+      t.rcov_dir = PROJ.rcov_dir
       t.rcov_opts = PROJ.rcov_opts + ['--exclude', 'spec']
+    end
+
+    RCov::VerifyTask.new(:verify) do |t| 
+      t.threshold = PROJ.rcov_threshold
+      t.index_html = File.join(PROJ.rcov_dir, 'index.html')
+      t.require_exact_threshold = PROJ.rcov_threshold_exact
     end
   end
 
