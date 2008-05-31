00eb7421b1d99604d36547bcd97e3e3b963dbcf9
Tim Pease <tim.pease@gmail.com>
2008-05-30 21:22:37 -0600

    Checking in what I've done with the custom Textile tags. It works mostly, but I've not found a way to prevent the inserted code from being processed by further textile markup rules. That is, if the inserted text contains a phrase like *NUMBER*, then it will have span tags inserted around it. This is not the desired behavior for code blocks.
    However, there are some good changes that will get pulled back into the master
    branch, but some cleanup is needed first.

--- lib/webby/filters/textile.rb
+++ lib/webby/filters/textile.rb
@@ -1,8 +1,10 @@
 try_require 'redcloth'
 
 # Render text via textile using the RedCloth library.
-Webby::Filters.register :textile do |input|
-  RedCloth.new(input, %w(no_span_caps)).to_html
+Webby::Filters.register :textile do |input, cursor|
+  rc = RedCloth.new(input, %w(no_span_caps))
+  rc.instance_variable_set(:@_renderer, cursor.renderer)
+  rc.to_html
 end
 
 # EOF
--- lib/webby/helpers/textile_code_helper.rb
+++ lib/webby/helpers/textile_code_helper.rb
@@ -5,8 +5,37 @@ require 'htmlentities'
 module Webby::Helpers
 module TextileCode
 
+  # This method defines a "code." tag in textile documents. The tag is used
+  # to apply UltraViolet syntax highlighting to a chunk of text. The text
+  # can be either inlined in the document or slurped in from a Webby
+  # partial.
   #
+  # ==== Examples
   #
+  # In this example, the site-wide default settings are used to determine
+  # language, line numbering and UV theme.
+  #
+  #   code. 
+  #   class Object
+  #     def returning( obj, &block )
+  #       block.call obj
+  #       return obj
+  #     end
+  #   end
+  #
+  # Options can be passed to the code. tag itself using Ruby hash sytax.
+  #
+  #   code. {:lang => 'ragel', :line_numbers => false}
+  #   action dgt      { printf("DGT: %c\n", fc); }
+  #   action dec      { printf("DEC: .\n"); }
+  #   action exp      { printf("EXP: %c\n", fc); }
+  #   action exp_sign { printf("SGN: %c\n", fc); }
+  #   action number   { /*NUMBER*/ }
+  #   number = (
+  #       [0-9]+ $dgt ( '.' @dec [0-9]+ $dgt )?
+  #           ( [eE] ( [+\-] $exp_sign )? [0-9]+ $exp )?
+  #           ) %number;
+  #   main := ( number '\n' )*;
   #
   def textile_code( tag, atts, cite, content )
     rgxp = %r/class="([^\"]*)"/
@@ -17,16 +46,29 @@ module TextileCode
            end
 
     # remove the first line of text from the content
-    content = content.sub(%r/^([^#$/]*)#$//m, '')
+    content = content.sub(%r/^([^#$/]*)#$/?/m, '')
     header = $1.strip
 
-    # try to match for an external "filename[part] {opts}"
-    m = %r/([^\[\{]*)?(?:\[([^\{]*)\])?\s*(\{.*\})?/.match header
-    filename, part, opts = m.to_a.last(3)
+    # try to match for an external "partial {opts}"
+    m = %r/([^\{]*)?\s*(\{.*\})?/.match header
+    filename, opts = m.to_a.last(2)
 
     # grab the options to pass to the UltraViolet syntax highlighter
     opts = opts ? eval(opts) : {}
     defaults = ::Webby.site.uv
+
+    # try to find the partial if a filename was given
+    unless filename.empty?
+      partial = @_renderer._find_partial(filename.strip)
+      content << @_renderer.render_partial(partial)
+
+      h = {}
+      %w[lang line_numbers theme].each do |key|
+        v = partial[key]
+        h[key.to_sym] = v unless v.nil?
+      end
+      defaults = defaults.merge(h)
+    end
 
     lang = opts.getopt(:lang, defaults[:lang])
     line_numbers = opts.getopt(:line_numbers, defaults[:line_numbers])
@@ -46,6 +88,9 @@ module TextileCode
   end
 end  # module TextileCode
 
+# FIXME: it appears that RedCloth is going to insert span tags into the rendered code blocks
+#         -- so I'm removing this functionality for# the time being (which makes me very sad)
+#
 RedCloth.__send__(:include, TextileCode)
 
 end  # module Webby::Helpers
