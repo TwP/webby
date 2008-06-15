3ec6ba764e00e187dcd01d6f195c5db4f3ad32b2
Tim Pease <tim.pease@gmail.com>
2008-06-14 21:37:22 -0600

    Initial support for the rake hooks.

--- lib/webby/generator.rb
+++ lib/webby/generator.rb
@@ -1,10 +1,9 @@
-require 'filutils'
+require 'fileutils'
 require 'optparse'
 
 module Webby
 
-# webby gen template dest  => creates the tmplate
-# webby gen templates      => lists available templates
+# webby gen template site  => creates the tmplate
 # webby gen -h / --help
 # webby gen       => same as --help
 #
@@ -14,51 +13,57 @@ class Generator
   WINDOWS = %r/djgpp|(cyg|ms|bcc)win|mingw/ =~ RUBY_PLATFORM    # :nodoc:
 
   attr_accessor :template, :site
+  attr_reader :options
 
+  # Initialize a new generator object.
+  #
   def initialize
-    @force = @update = @pretend = false
+    @options = {}
     @site = @template = nil
+    @stdout, @stdin = $stdout, $stdin
   end
 
-  %w[update force pretend].each do |m|
-    class_eval "def #{m}?() @#{m}; end"
-  end
-
+  # Run the generator executing the commands specified by the user on the
+  # command line.
+  #
   def run( args )
     parse args
     create_site
   end
 
+  # Parse out the command line options found in the args array.
+  #
   def parse( args )
     opts = OptionParser.new
-    opts.banner << ' gen [template] [site]'
+    opts.banner = 'Usage: webby gen [options] template site'
 
     opts.separator ''
     opts.on('-f', '--force',
-            'overwrite files that already exist') {@force = true}
+            'overwrite files that already exist') {options[:collision] = :force}
     opts.on('-s', '--skip',
-            'skip files that already exist') {@force = true}
+            'skip files that already exist') {options[:collision] = :skip}
     opts.on('-u', '--update',
-            'update rake tasks for the site') {@update = true}
+            'update rake tasks for the site') {options[:update] = true}
     opts.on('-p', '--pretend',
-            'run but do not make any changes') {@pretend = true}
+            'run but do not make any changes') {options[:pretend] = true}
 
     opts.separator ''
     opts.on('-t', '--templates', 'list available templates') {
-      puts opts
+      @stdout.puts opts
       ary = templates.map {|t| ::File.basename(t)}
-      ary.delete! 'webby'
-      puts "\nAvailable Templates"
-      puts "    #{ary.join(', ')}\n"
+      ary.delete 'webby'
+      @stdout.puts "\nAvailable Templates"
+      @stdout.puts "    #{ary.join(', ')}"
+      @stdout.puts
       exit
     }
 
     opts.separator ''
     opts.separator 'common options:'
 
-    opts.on_tail( '-h', '--help', 'show this message' ) {puts opts; exit}
+    opts.on_tail( '-h', '--help', 'show this message' ) {@stdout.puts opts; exit}
     opts.on_tail( '--version', 'show version' ) do
-      puts "Webby #{::Webby::VERSION}"
+      @stdout.puts "Webby #{::Webby::VERSION}"
       exit
     end
 
@@ -67,17 +72,25 @@ class Generator
     tmpl, @site = args
 
     if @site.nil? or tmpl.nil?
-      puts opts
+      @stdout.puts opts
       exit 1
     end
 
     templates.each {|t| self.template = t if t =~ %r/\/#{tmpl}$/}
     if @template.nil?
-      puts opts
+      @stdout.puts opts
       abort "Could not find template '#{tmpl}'"
     end
 
     nil
+  end
+
+  # Returns +true+ if we are only going to pretend to do something. All the
+  # output messages will be written, but no changes will be made on the
+  # disc.
+  #
+  def pretend?
+    options[:pretend] == true
   end
 
   # Returns an array of available site templates.
@@ -86,23 +99,28 @@ class Generator
     Dir.glob(::Webby.path('examples') / '*').sort
   end
 
-  #
+  # Create the site from the template specified by the user.
   #
   def create_site
-    unless test ?e, site
-      FileUtils.mkdir_p site unless pretend?
-    end
-
     files = site_files
 
-    if update?
+    # in update mode we only want to update the tasks directory
+    if options[:update]
+      FileUtils.mkdir_p site unless pretend?
       mkdir 'tasks'
       files['tasks'].sort.each {|file| cp file}
     else
       dirs = files.keys.sort
 
-      dirs.each {|dir| mkdir dir}
+      # create the directories first
       dirs.each do |dir|
+        next if dir =~ %r/^output\/.*$/
+        mkdir dir
+      end
+
+      # and the create the files under each directory
+      dirs.each do |dir|
+        next if dir =~ %r/^output(\/.*)?$/
         files[dir].sort.each {|file| cp file}
       end
     end
@@ -130,14 +148,20 @@ class Generator
     dst = site / file
 
     if test(?e, dst) 
-      # TODO: test for identical files
-      if    force? then force(dst)
-      elsif skip?  then skip(dst); return
-      else
-      # TODO: query the user for what to do with this file
-        skip(dst)
+      if identical?(src, dst)
+        identical(dst)
         return
       end
+
+      choice = case options[:collision]
+        when :force then :force
+        when :skip  then :skip
+        else force_file_collision?( dst ) end
+
+      case choice
+        when :force then force(dst)
+        when :skip  then skip(dst); return
+        else raise "Invalid collision choice: #{choice.inspect}" end
     else
       create(dst)
     end
@@ -165,19 +189,24 @@ class Generator
   end
 
   %w[exists create force skip identical].each do |m|
-    class_eval "def #{m}( msg ) message(#{m}, msg); end"
+    class_eval "def #{m}( msg ) message('#{m}', msg); end"
   end
 
+  # Print the given message and message type to stdout.
+  #
   def message( type, msg )
     msg = msg.sub(%r/#{site}\/?/, '')
-    puts "%13s  %s" % [type, msg]
+    return if msg.empty?
+    @stdout.puts "%13s  %s" % [type, msg]
   end
 
   # Prints an abort message to the screen and then exits the Ruby
   # interpreter. A non-zero return code is used to indicate an error.
   #
   def abort( msg )
-    puts "\n    #{msg}\n"
+    @stdout.puts "\nAborting!"
+    @stdout.puts "    #{msg}"
+    @stdout.puts
     exit 1
   end
 
@@ -202,6 +231,33 @@ class Generator
     end
 
     paths
+  end
+
+  # Returns +true+ if the source file is identical to the destination file.
+  # Returns +false+ if this is not the case.
+  #
+  def identical?( src, dst )
+    # FIXME: this most likely won't work on windows machines
+    #        because the line endings are modified when the site is gnerated
+    source      = IO.read(src)
+    destination = IO.read(dst)
+    source == destination
+  end
+
+  # Ask the user what to do about the file collision.
+  #
+  def force_file_collision?( dst )
+    dst = dst.sub(%r/#{site}\/?/, '')
+    @stdout.print "overwrite #{dst}? [(Y)es (n)o (q)uit] "
+    @stdout.flush
+    case @stdin.gets
+      when %r/q/i  then abort 'user asked to quit'
+      when %r/n/i  then :skip
+      when %r/y/i  then :force
+      when %r/\s*/ then :force
+      else force_file_collision?(dst) end
+  rescue
+    retry
   end
 
 end  # class Generator
--- lib/webby/main.rb
+++ lib/webby/main.rb
@@ -1,6 +1,5 @@
-require 'fileutils'
-require 'find'
 require 'optparse'
+require 'rake'
 
 module Webby
 
@@ -9,216 +8,45 @@ module Webby
 #
 class Main
 
-  WINDOWS = %r/djgpp|(cyg|ms|bcc)win|mingw/ =~ RUBY_PLATFORM    # :nodoc:
-
-  # Directory where the Webby website will be created
-  attr_accessor :site
-
-  # Directory where the prototype Webby website can be found
-  attr_accessor :data
-
-  # Flag used to update an existing website
-  attr_accessor :update
-
-  # call-seq:
-  #    Main.run( args )    => nil
-  #
   # Create a new instance of Main, and run the +webby+ application given the
   # command line _args_.
   #
   def self.run( args )
-    m = self.new
-    m.parse args
-
-    if m.update then m.update_site
-                else m.create_site end
+    self.new.run args
   end
 
-  # call-seq:
-  #    Main.new
-  #
   # Create a new Main webby object for building websites.
   #
   def initialize
-    @log = Logging::Logger[self]
+    @stdout = $stdout
   end
 
-  # call-seq:
-  #    parse( args )   => nil
-  #
-  # Parse the command line arguments and store the values for later use by
-  # the create_site and update_site methods.
-  #
-  def parse( args )
-    self.data = File.join(::Webby::PATH, 'data')
-    self.update = false
-
-    opts = OptionParser.new
-    opts.banner << ' site'
-
-    opts.separator ''
-    opts.on('-u', '--update',
-            'update the rake tasks for the site') {self.update = true}
-
-    opts.separator ''
-    opts.separator 'common options:'
-
-    opts.on_tail( '-h', '--help', 'show this message' ) {puts opts; exit}
-    opts.on_tail( '--version', 'show version' ) do
-      puts "Webby #{::Webby::VERSION}"
-      exit
-    end
-
-    # parse the command line arguments
-    opts.parse! args
-    self.site = args.shift
-
-    if site.nil?
-      puts opts
-      ::Kernel.abort
-    end
-    nil
+  def run( args )
+    case args[0]
+    when 'gen'
+      args.shift
+      gen = Generator.new
+      gen.run args
+    when nil              then help
+    when %r/^(-h|--help)/ then help
+    when '--version'      then version
+    else rake(args) end
   end
 
-  # call-seq:
-  #    create_site    => nil
-  #
-  # Create a new website.
-  #
-  def create_site
-    # see if the site already exists
-    abort "'#{site}' already exists" if test ?e, site
-
-    # copy over files from the data directory
-    files = site_files
-
-    files.keys.sort.each do |dir|
-      mkdir dir
-      files[dir].sort.each {|file| cp file}
-    end
-    nil
+  def rake( args )
+    # TODO: fill in the rake stuff
+    @stdout.puts 'rake'
   end
 
-  # call-seq:
-  #    update_site    => nil
-  #
-  # Update the rake tasks for an existing website.
-  #
-  def update_site
-    # ensure the site already exists
-    abort "'#{site}' does not exist" unless test ?d, site
-
-    # copy over files from the data/tasks directory
-    files = site_files
-
-    mkdir 'tasks'
-    files['tasks'].sort.each {|file| cp file}
-
-    nil
+  def version
+    @stdout.puts "Webby #{::Webby::VERSION}"
   end
 
-  # call-seq:
-  #    mkdir( dir )    => nil
-  #
-  # Make a directory in the user specified site location. A message will be
-  # displayed to the screen indicating tha the directory is being created.
-  #
-  def mkdir( dir )
-    dir = dir.empty? ? site : ::File.join(site, dir)
-    unless test ?d, dir
-      creating dir
-      FileUtils.mkdir_p dir
-    end
-  end
-
-  # call-seq:
-  #    cp( file )    => nil
-  #
-  # Copy a file from the Webby prototype website location to the user
-  # specified site location. A message will be displayed to the screen
-  # indicating tha the file is being created.
-  #
-  def cp( file )
-    src = ::File.join(data, file)
-    dst = ::File.join(site, file)
-    test(?e, dst) ? updating(dst) : creating(dst)
-
-    if WINDOWS then win_line_endings(src, dst)
-    else FileUtils.cp(src, dst) end
-  end
-
-  # call-seq:
-  #    win_line_endings( src, dst )
-  #
-  # Copy the file from the _src_ location to the _dst_ location and
-  # transform the line endings to the windows "\r\n" format.
-  #
-  def win_line_endings( src, dst )
-    case ::File.extname(src)
-    when *%w[.png .gif .jpg .jpeg]
-      FileUtils.cp src, dst
-    else
-      ::File.open(dst,'w') do |fd|
-        ::File.foreach(src, "\n") do |line|
-          line.tr!("\r\n",'')
-          fd.puts line
-        end
-      end
-    end
-  end
-
-  # call-seq:
-  #    creating( msg )   => nil
-  #
-  # Prints a "creating _msg_" to the screen.
-  #
-  def creating( msg )
-    @log.info "creating #{msg}"
-  end
-
-  # call-seq:
-  #    updating( msg )   => nil
-  #
-  # Prints a "updating _msg_" to the screen.
-  #
-  def updating( msg )
-    @log.info "updating #{msg}"
-  end
-
-  # call-seq:
-  #    abort( msg )   => nil
-  #
-  # Prints an abort _msg_ to the screen and then exits the Ruby interpreter.
-  #
-  def abort( msg )
-    @log.fatal msg
-    exit 1
-  end
-
-  # call-seq:
-  #    site_files   => hash
-  #
-  # Iterates over all the files in the Webby prototype website directory and
-  # stores them in a hash.
-  #
-  def site_files
-    exclude = %r/tmp$|bak$|~$|CVS|\.svn/o
-
-    rgxp = %r/\A#{data}\/?/o
-    paths = Hash.new {|h,k| h[k] = []}
-
-    Find.find(data) do |p|
-      next if exclude =~ p
-
-      if test(?d, p)
-        paths[p.sub(rgxp, '')]
-        next
-      end
-      dir = ::File.dirname(p).sub(rgxp, '')
-      paths[dir] << p.sub(rgxp, '')
-    end
-
-    paths
+  def help
+    # TODO: get a good help message going
+    #       maybe use the option parser just for the help message
+    @stdout.puts "Usage: webby [options] target [target args]"
+    @stdout.puts
   end
 
 end  # class Main
